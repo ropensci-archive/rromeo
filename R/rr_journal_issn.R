50975fb1c1a167e47fd927534e99b822d677c962
Rekyt <matthias.grenie@ens-lyon.fr>
2019-09-30 13:46:49 +0200

    Mention ESSN in doc & vignette

--- R/rr_journal_issn.R
+++ R/rr_journal_issn.R
@@ -1,10 +1,11 @@
 #' Retrieve journal policy using ISSN
 #'
-#' Retrieve policy information from the SHERPA/RoMEO API using the ISSN of
-#' the journal
+#' Retrieve policy information from the SHERPA/RoMEO API using the ISSN
+#' from the paper edition of the journal or the ISSN of the electronic version
+#' (e-ISSN or ESSN)
 #'
 #' @param issn \[`character(1+)`\]\cr{}
-#'             one or a vector of journal(s) ISSN(s)
+#'             one or a vector of journal(s) ISSN(s) or ESSN(s)
 #' @inheritParams check_key
 #'
 #' @inherit rr_journal_name return
@@ -14,8 +15,14 @@
 #' @export
 #'
 #' @examples \donttest{
+#' # Query single ISSN
 #' rr_journal_issn(issn = "1947-6264")
+#'
+#' # Query multiple ISSN
 #' rr_journal_issn(issn = c("1947-6264", "0030-1299"))
+#'
+#' # Query by ESSN
+#' rr_journal_issn("1463-9084")
 #' }
 rr_journal_issn <- function(issn, key = NULL) {
 
