4ad51630ed5a3e9bed0129d8df9b0980489083aa
Rekyt <matthias.grenie@ens-lyon.fr>
2020-03-06 09:29:08 +0100

    Remove rr_romeo_colour() test

--- tests/testthat/test-rr_romeo_colour.R
+++ tests/testthat/test-rr_romeo_colour.R
@@ -5,24 +5,4 @@ test_that("rr_romeo_colour() works", {
                regexp = paste0("'arg' should be one of ", dQuote("green"), ", ",
                               dQuote("blue"), ", ", dQuote("yellow"), ", ",
                               dQuote("white")))
-
-
-
-  use_cassette("rr_romeo_colour", {
-    res <- rr_romeo_colour("yellow")
-
-    expect_is(res, "data.frame")
-
-
-    expect_named(res, c("romeoid", "publisher", "alias", "romeocolour",
-                        "preprint", "postprint", "pdf"))
-
-    expect_is(res$romeoid,     "numeric")
-    expect_is(res$publisher,   "character")
-    expect_is(res$alias,       "character")
-    expect_is(res$romeocolour, "character")
-    expect_is(res$preprint,    "character")
-    expect_is(res$postprint,   "character")
-    expect_is(res$pdf,         "character")
-  })
 })
